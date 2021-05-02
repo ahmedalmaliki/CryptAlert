8c63577e45df7535c364a15a90ecf57cbe1a9fac
Ahmed Al-maliki <ahmedalmalikiab@gmail.com>
2021-05-02 20:40:51 +0300

    First commit

---
+++ app/src/main/java/com/example/cryptalert/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.cryptalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        showPrice();
+    }
+
+
+
+    private void showPrice() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://www.okex.com/api/spot/v3/instruments/BTC-USDT/ticker";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+}
